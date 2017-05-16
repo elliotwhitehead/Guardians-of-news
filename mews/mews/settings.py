b5b0abc4011b84db125e7f6e4422c818d158adbf
Elliot Whitehead <elliotwhitehead1@yahoo.com>
2017-05-15 20:04:06 -0600

    Add tempates and routes for homepage

--- mews/mews/settings.py
+++ mews/mews/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+FILES_DIR = os.path.join(BASE_DIR, 'files')
+
+TEMPLATES_DIR = os.path.join(FILES_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'mews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
